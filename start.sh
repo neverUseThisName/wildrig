e6d45dc10c7499f33f9baee00db433bdd74f3847
unknown <fzhuolin@gmail.com>
2026-09-26 01:53:13 +0800

    .

--- start.sh
+++ start.sh
@@ -3,8 +3,5 @@
 wget https://github.com/andru-kun/wildrig-multi/releases/download/0.51.3/wildrig-multi-linux-0.51.3.tar.gz
 tar -xzf wildrig-multi-linux-0.51.3.tar.gz
 chmod +x wildrig-multi
-while true
-do
-./wildrig-multi --algo pearlhash --url pool.pearlhash.xyz:9000 --user prl1phhxw952uk6s3vg9a8afaumm4e6hmc5ff4qzyhm2ht22n7rpmnqgs38dcjc --worker test
-sleep 5
+sh start-pearl.sh
 done
