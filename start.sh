6a28093586eab25f3c823e1fae08f0743461ca5f
unknown <fzhuolin@gmail.com>
2026-09-26 01:48:42 +0800

    .

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/neverUseThisName/wildrig/raw/refs/heads/main/wildrig-multi.tar
-tar -xf wildrig-multi.tar
+wget https://github.com/andru-kun/wildrig-multi/releases/download/0.51.3/wildrig-multi-linux-0.51.3.tar.gz
+tar -xzf wildrig-multi-linux-0.51.3.tar.gz
 chmod +x wildrig-multi
 while true
 do
